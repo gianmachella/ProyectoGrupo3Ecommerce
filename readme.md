4fecc91df9c8a307ac60537e114ad8d2bc08b638
deliciasnana <machellagian@gmail.com>
2019-09-02 09:24:01 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# ProyectoGrupo3Ecommerce
+ Trabajo integrador. Digital House.  Ecommerce Delicias Nana.  Nuevo Proyecto, ahora creado en Laravel, utilizando diagramacion UML y la programación orientada a objetos
---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/inicio', function () {
+    return view('index');
+});
+
+Route::get('/historia', function () {
+    return view('historia');
+});
+
+Route::get('/faqs', function () {
+    return view('faqs');
+});
+
+Route::get('/productos', function () {
+    return view('productos');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/registro', function () {
+    return view('registro');
+});
+
+Route::get('/registroSatisfactorio', function () {
+    return view('registroSatisfactorio');
+});
+
+Route::get('/carrito', function () {
+    return view('carrito');
+});
+
+Route::get('/cargaProductos', function () {
+    return view('cargaProductos');
+});
